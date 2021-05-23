f54c906a2811d3e2903c7a3bdd95cabe12b1bdbc
Marc Bourgoin <themard85@gmail.com>
2021-05-23 07:56:53 -0600

    Z01RD: Switch to source-built bootctl and update_engine_sideload
    Change-Id: Ic22e31d31d2e2ddcb75160d2ef2b49733ba6b6dc

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier
 
+PRODUCT_PACKAGES += \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    update_engine_sideload
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -58,6 +62,16 @@ TARGET_SCREEN_WIDTH := 1080
 # tzdata
 PRODUCT_PACKAGES += \
     tzdata_twrp
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
